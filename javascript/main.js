d56446e889804a643744f36cbddbbcf4875cde38
Divyanshi Rajput <56169692+DivyanshiRajput@users.noreply.github.com>
2020-09-11 13:10:27 +0530

    Added the function sendURL

--- javascript/main.js
+++ javascript/main.js
@@ -1,43 +1,45 @@
 
-
-
 document.querySelector(".js-go").addEventListener('click', function(){
 	var input = document.querySelector("input").value;
-	pushToDOM(input);
+	var url = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + input; 
+	sendURL(url);
 });
+
 
 document.querySelector(".js-userinput").addEventListener('keyup', function(e){
 	var input = document.querySelector("input").value;
-	if (e.which == 13){
-	pushToDOM(input);		
+	var url = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + input; 
+	if (e.which == 13)
+	{
+		sendURL(url);		
 	}
 
 });
 
-var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
 
-var GiphyAJAXCall = new XMLHttpRequest();
-GiphyAJAXCall.open("GET", url);
-GiphyAJAXCall.send();
+function sendURL (url)
+{
+	var GiphyAJAXCall = new XMLHttpRequest();
+	GiphyAJAXCall.open("GET", url);
+	GiphyAJAXCall.send();
 
-GiphyAJAXCall.addEventListener("load", function(e){
-	var data = e.target.response ;
-	pushToDOM(data);
-})
+	GiphyAJAXCall.addEventListener("load", function(e){
+		var data = e.target.response ;
+		pushToDOM(data);
+	})
+}
 
-function pushToDOM(input){
 
+function pushToDOM(input)
+{
 	var response = JSON.parse(input);
 	var container = document.querySelector(".js-container");
 	var imageurls = response.data;
+	container.innerHTML = "";
 	imageurls.forEach(function(image){
 
 		var src = image.images.fixed_height.url;
 		
 		container.innerHTML += "<img src=\"" + src + "\" class=\"container-image\">";
-
-
 	});
-
-	
 }
